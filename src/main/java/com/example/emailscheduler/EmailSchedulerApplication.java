13b6afe137ead15387fc2358c9ccbee676952ed8
Animesh <animsingh56@gmail.com>
2022-01-08 19:46:33 +0530

    Updated the port number in properties file to 8081

--- src/main/java/com/example/emailscheduler/EmailSchedulerApplication.java
+++ src/main/java/com/example/emailscheduler/EmailSchedulerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmailSchedulerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmailSchedulerApplication.class, args);
 	}
 
